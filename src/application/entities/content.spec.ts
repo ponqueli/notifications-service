38c0f0ca166efe11ead9694bfed24154b9305616
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-12-24 19:07:04 -0300

    add notification test

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,17 +1,20 @@
 import { Content } from './content';
-test('it should be able to create a notification content', () => {
-  const content = new Content('Você recebeu uma solicitação de amizade');
-  expect(content).toBeTruthy();
-});
 
-test('it should not be able to create a notification content with last than 5 characters', () => {
-  expect(() => {
-    new Content('1234');
-  }).toThrow();
-});
+describe('Notification Content', () => {
+  it('should be able to create a notification content', () => {
+    const content = new Content('Você recebeu uma solicitação de amizade');
+    expect(content).toBeTruthy();
+  });
 
-test('it should not be able to create a notification content with more than 240 characters', () => {
-  expect(() => {
-    new Content('3'.repeat(241));
-  }).toThrow();
+  it('should not be able to create a notification content with last than 5 characters', () => {
+    expect(() => {
+      new Content('1234');
+    }).toThrow();
+  });
+
+  it('should not be able to create a notification content with more than 240 characters', () => {
+    expect(() => {
+      new Content('3'.repeat(241));
+    }).toThrow();
+  });
 });
