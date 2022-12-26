137a82513f6dfc2252806e3d9f9d6f6b978c4bad
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-12-26 14:50:46 -0300

    test cancel notif does not exist

--- src/application/use-cases/cancel-notifications.spec.ts
+++ src/application/use-cases/cancel-notifications.spec.ts
@@ -2,6 +2,7 @@ import { Notification } from './../entities/notification';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CancelNotification } from './cancel-notification';
 import { Content } from '@application/entities/content';
+import { NotificationNotFound } from './errors/notification-not-found';
 
 describe('Cancel notification', () => {
   it('should be able to cancel a notification', async () => {
@@ -20,6 +21,18 @@ describe('Cancel notification', () => {
     })
 
     expect(notificationsRepository.notifications[0].cancelledAt).toEqual(expect.any(Date));
+
+  });
+
+  it('should not be able to cancel a notification that does not exist', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'non-existing-notification-id'
+      });
+    }).rejects.toThrow(NotificationNotFound)
 
   });
 });
