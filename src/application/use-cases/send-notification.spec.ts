dbfd7d99911316ca2d1da740899d2bc37be4e5e7
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-12-25 23:45:52 -0300

    configuring typescript path

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,8 +1,8 @@
-import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { SendNotification } from './send-notification';
 
 describe('Send notification', () => {
-  it('should be able to send a notification', async ()=>{
+  it('should be able to send a notification', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const sendNotification = new SendNotification(notificationsRepository);
     const { notification } = await sendNotification.execute({
