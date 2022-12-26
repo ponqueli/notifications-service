f06366de29e394e31889f47de34634fb942a889a
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-12-26 16:39:24 -0300

    create factory pattern to evoid duplication calls

--- src/application/use-cases/count-recipient-notifications.spec.ts
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -1,36 +1,15 @@
 import { CountRecipientNotifications } from './count-recipient-notifications';
-import { Notification } from './../entities/notification';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
-import { Content } from '@application/entities/content';
+import { makeNotification } from '@test/factories/notification-factory';
 
 describe('Count recipients notifications', () => {
   it('should be able to count recipient notifications', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const countRecipientNotifications = new CountRecipientNotifications(notificationsRepository);
 
-    await notificationsRepository.create(
-      new Notification({
-        category: 'social',
-        content: new Content('Nova solicitação de amizade'),
-        recipientId: 'recipient-1'
-      })
-    );
-
-    await notificationsRepository.create(
-      new Notification({
-        category: 'social',
-        content: new Content('Nova solicitação de amizade 2'),
-        recipientId: 'recipient-1'
-      })
-    );
-
-    await notificationsRepository.create(
-      new Notification({
-        category: 'social',
-        content: new Content('Nova solicitação de amizade 2'),
-        recipientId: 'recipient-2'
-      })
-    );
+    await notificationsRepository.create(makeNotification({ recipientId: 'recipient-1' }));
+    await notificationsRepository.create(makeNotification({ recipientId: 'recipient-1' }));
+    await notificationsRepository.create(makeNotification({ recipientId: 'recipient-2' }));
 
     const { count } = await countRecipientNotifications.execute({
       recipientId: 'recipient-1'
