6a080b99504b6766662533bd773cf045b4b9d528
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-12-25 21:16:30 -0300

    creating the contract, interface, to get notification saved

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,14 +1,24 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications : Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification){
+    notifications.push(notification);
+  }
+}
 
 describe('Send notification', () => {
   it('should be able to send a notification', async ()=>{
-    const sendNotification = new SendNotification();
-    const { notification } = await sendNotification.execute({
+    const sendNotification = new SendNotification(notificationsRepository);
+    await sendNotification.execute({
       category: 'social',
       content: 'This is a notification',
       recipientId: 'example-recipient-id',
     });
 
-    expect(notification).toBeTruthy();
+    console.log(notifications)
+    expect(notifications).toHaveLength(1);
   });
 });
