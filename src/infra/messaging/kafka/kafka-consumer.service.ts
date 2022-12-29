206c7a66687b7e96bcc53f8d30991add0d4fb5ea
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-12-28 23:19:11 -0300

    add kafka to the microservice

---
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { ServerKafka } from '@nestjs/microservices';
+
+@Injectable()
+export class KafkaConsumerService extends ServerKafka implements OnModuleDestroy {
+  constructor() {
+    super({
+      client: {
+        clientId: 'notifications',
+        brokers: ['wealthy-duck-6474-us1-kafka.upstash.io:9092'],
+        sasl: {
+          mechanism: 'scram-sha-256',
+          username: 'd2VhbHRoeS1kdWNrLTY0NzQkdNUHNZ2s8KFYXSNkTfhDkzqauqdfSBrTJxBirFg',
+          password: '',
+        },
+        ssl: true,
+      }
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.close();
+  }
+
+}
---
+++ src/infra/messaging/messaging.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { KafkaConsumerService } from './kafka/kafka-consumer.service';
+
+@Module({
+  imports: [],
+  providers: [KafkaConsumerService],
+  controllers: [],
+})
+export class MessagingModule {
+
+}
