1af5405f6147ace8d60462110a9a638b3fb45a32
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-12-29 22:00:04 -0300

    creating notification and saving in database from producer

--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,10 +1,12 @@
+import { SendNotification } from '@application/use-cases/send-notification';
 import { NotificationsController } from './kafka/controllers/notifications.controller';
 import { Module } from '@nestjs/common';
 import { KafkaConsumerService } from './kafka/kafka-consumer.service';
+import { DatabaseModule } from '@infra/database/database.module';
 
 @Module({
-  imports: [],
-  providers: [KafkaConsumerService],
+  imports: [DatabaseModule],
+  providers: [KafkaConsumerService, SendNotification],
   controllers: [NotificationsController],
 })
 export class MessagingModule {
